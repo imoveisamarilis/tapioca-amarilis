42b58e6c2cc7ea629ce4672fa0487594dc319d6a
Daniel Bastos <danielfloresbastos@gmail.com>
2017-08-07 10:17:48 -0300

    add bumpversion

--- setup.py
+++ setup.py
@@ -27,53 +27,16 @@ test_requirements = [
 ]
 
 
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `init.py`.
-    """
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("^__version__ = ['\"]([^'\"]+)['\"]",
-                     init_py, re.MULTILINE).group(1)
-
-
-def get_author(package):
-    """
-    Return package author as listed in `__author__` in `init.py`.
-    """
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("^__author__ = ['\"]([^'\"]+)['\"]",
-                     init_py, re.MULTILINE).group(1)
-
-
-def get_email(package):
-    """
-    Return package email as listed in `__email__` in `init.py`.
-    """
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("^__email__ = ['\"]([^'\"]+)['\"]",
-                     init_py, re.MULTILINE).group(1)
-
-
-# python setup.py register
-if sys.argv[-1] == 'publish':
-    os.system("python setup.py sdist upload")
-    args = {'version': get_version(package)}
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
-    print("  git push --tags")
-    sys.exit()
-
-
 setup(
     name='tapioca-amarilis',
-    version=get_version(package),
+    version='0.0.0',
     description='Amarilis API wrapper using tapioca',
     long_description=readme,
-    author=get_author(package),
-    author_email=get_email(package),
+    author='Daniel Bastos',
+    author_email='danielfloresbastos@gmail.com',
     url='https://github.com/daniellbastos/tapioca-amarilis',
     packages=[
-        'tapioca_amarilis',
+     'tapioca_amarilis',
     ],
     package_dir={'tapioca_amarilis': 'tapioca_amarilis'},
     include_package_data=True,
--- tapioca_amarilis/__init__.py
+++ tapioca_amarilis/__init__.py
@@ -1,8 +1,1 @@
-# coding: utf-8
-
-__author__ = 'Daniel Bastos'
-__email__ = 'danielfloresbastos@gmail.com'
-__version__ = '0.1.0'
-
-
 from .tapioca_amarilis import AmarilisV1  # noqa
